7fb2cef6b0e2030359ed71cd85aa5fc8bf29a298
PTC Crypto <ptccrypto@PTCs-MacBook-Air.local>
2025-12-02 15:01:29 -0500

    Fix Ostium URL - use ostium.com not ostium.io

--- src/app/speculate/avantis/page.tsx
+++ src/app/speculate/avantis/page.tsx
@@ -5,7 +5,7 @@ import { EmbeddedDApp } from '@/components/embed/EmbeddedDApp'
 export default function OstiumPage() {
   return (
     <EmbeddedDApp
-      url="https://app.ostium.io/trade"
+      url="https://app.ostium.com/trade"
       name="Ostium"
       description="Forex & RWA perps on Arbitrum"
       backHref="/speculate"
