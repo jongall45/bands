b7cbc490c35a1c8004ccb32e21f777d2705a28a8
PTC Crypto <ptccrypto@PTCs-MacBook-Air.local>
2025-12-04 17:06:11 -0500

    fix: Remove SmartOstiumTrader - requires unsupported Privy smart wallets

--- src/features/trading/index.ts
+++ src/features/trading/index.ts
@@ -3,5 +3,5 @@ export { useSmartApproval, USDC_ARBITRUM, OSTIUM_TRADING } from './useSmartAppro
 export { OstiumExecutionButton, USDC_ADDRESS, OSTIUM_SPENDER, USDC_DECIMALS } from './OstiumExecutionButton'
 export { TradeActionButtons } from './TradeActionButtons'
 export { useOstiumAllowanceStrict, OSTIUM_TRADING_ADDRESS } from './useOstiumAllowanceStrict'
-export { SmartOstiumTrader } from './SmartOstiumTrader'
+// SmartOstiumTrader removed - requires Privy smart wallets which aren't available
 
